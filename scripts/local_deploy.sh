807dac34be7655e6811f2307bdefc10b4d00ed02
Prakash Bhatt <prakash@fueldao.io>
2024-12-16 13:38:22 +0530

    [skip ci] Added local deploy script

---
+++ scripts/local_deploy.sh
@@ -0,0 +1,4 @@
+dfx deploy --specified-id 4sc7j-7iaaa-aaaaj-qnfva-cai  asset
+dfx deploy --specified-id 43buv-jaaaa-aaaaj-qnfuq-cai  asset_proxy
+dfx deploy --specified-id 44asb-eyaaa-aaaaj-qnfua-cai  provision
+dfx deploy --specified-id ewirk-vqaaa-aaaaj-qa57q-cai  backend
