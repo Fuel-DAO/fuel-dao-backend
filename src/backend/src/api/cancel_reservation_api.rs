0bcc20bc0dfca7a615d35e5da4c716b861c68cea
Prakash Bhatt <prakash@fueldao.io>
2025-01-07 21:45:39 +0530

    Updated cancel reservation API

--- src/backend/src/api/cancel_reservation_api.rs
+++ src/backend/src/api/cancel_reservation_api.rs
@@ -5,12 +5,16 @@ use crate::is_controller;
 #[update(guard = "is_controller")]
 fn cancel_reservation(car_id: u64, booking_id: u64) -> Result<String, String> {
     // let customer_id = ic_cdk::caller();
-    STATE.with(|state| {
-        let  state = &mut state.borrow_mut().cars.get(&car_id);
-        if let Some(car) = state {
-            let booking =  car.bookings.remove(&booking_id);
+    STATE.with_borrow_mut(|state| {
+        let  get_car =state.cars.get(&car_id);
+        if let Some(mut car) = get_car {
+            let booking =  car.bookings.get(&booking_id);
             match booking {
-                Some(_) =>  return Ok("Reservation cancelled".to_string()),
+                Some(_) => {
+                    car.bookings.remove(&booking_id);
+                    state.cars.insert(car_id,car.clone());
+                    return Ok("Reservation cancelled".to_string());
+                },
                 None => return Err("No active reservation found for this car".to_string()),
             }
         }
